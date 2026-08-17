5725603e3220ce04477963d672273073b9b8f290
Ijeoma Godspower <ejehijeoma197@gmail.com>
2026-08-17 05:53:55 +0100

    fix

--- src/Components/Herosection/Herosection.tsx
+++ src/Components/Herosection/Herosection.tsx
@@ -62,7 +62,7 @@ const Herosection = () => {
             >
               <a
                 href="https://wa.me/2348133235527"
-                className="flex flex-row w-full gap-22 md:40"
+                className="flex flex-row w-full gap-22 md:25"
                 target="_blank"
               >
                 Contact Me
