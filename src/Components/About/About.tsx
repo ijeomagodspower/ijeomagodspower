f60dd88e616ed57b4609bb112e55726412ecdb1f
Ijeoma Godspower <ejehijeoma197@gmail.com>
2026-08-18 19:53:55 +0100

    namespacing

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -4,7 +4,7 @@ import { FaCode } from "react-icons/fa";
 import ShinyText from "../../../@/components/ShinyText";
 
 const About = () => {
-  const myName = "IJEOMA &nbsp GODSPOWER";
+  const myName = "IJEOMA    GODSPOWER";
 
   return (
     <section
