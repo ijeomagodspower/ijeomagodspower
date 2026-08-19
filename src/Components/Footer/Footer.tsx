a1a4db5f261943cebeefdc5786bff8de346fcf3e
Ijeoma Godspower <ejehijeoma197@gmail.com>
2026-08-20 00:51:31 +0100

    fix

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -15,10 +15,10 @@ const Footer = () => {
 
   return (
     <section
-      className={`flex-col ${bgTheme} min-w-full py-6 px-4 space-y-4 align-center justify-center sm:flex-row sm:justify-between sm:px-10 `}
+      className={`flex-row ${bgTheme} min-w-full py-6 px-4 space-y-4 align-center justify-center sm:flex-row sm:justify-between sm:px-10 `}
       id="Footer"
     >
-      <div className="w-full flex-row space-y-4 justify-center ">
+      <div className="flex-row space-y-4 justify-center ">
         <h4 className="flex-row text-primary text-center sm:text-start">
           <span className="text-hover-primary font-bold">IJ • </span>©
           {new Date().getFullYear()} Ijeoma Godspower
