1c341840bd33024e16c65fb8a515f32a9f5b32b2
Ijeoma Godspower <ejehijeoma197@gmail.com>
2026-08-19 20:47:02 +0100

    fix

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -35,44 +35,48 @@ const Contact = () => {
       {/*Contact Context */}
       <div className="flex flex-col w-full bg-white/15 border-white/20 shadow-[0_8px_32px_rgba(31,38,135,0.15)] p-2 md-p-6 rounded-3xl">
         <div
-          className="flex flex-col w-full bg-lightbg rounded-3xl p-2 py-6 space-10
+          className="flex flex-col w-full bg-lightbg rounded-3xl p-2 pt-10 pb-6 space-10
            md:flex-row md:p-6 md:gap-10"
         >
           <div className="flex-col w-full md:w-[calc(100% - 12px)] ">
-            <ShinyText
-              text="Let's Work Together"
-              speed={2}
-              delay={0}
-              color="#904aff"
-              shineColor="#ffffff"
-              spread={120}
-              direction="left"
-              yoyo={false}
-              pauseOnHover={false}
-              disabled={false}
-              className="font-inter text-2xl font-bold md:hidden "
-            />
-            <ShinyText
-              text="Let's Work Together"
-              speed={2}
-              delay={0}
-              color="#904aff"
-              shineColor="#ffffff"
-              spread={120}
-              direction="left"
-              yoyo={false}
-              pauseOnHover={false}
-              disabled={false}
-              className="hidden font-inter text-5xl font-bold"
-            />
-            <div className="w-10 border-t-2 border-dashed mt-4 mb-8 border-secondary"></div>
+            <div className="md:hidden">
+              <ShinyText
+                text="Let's Work Together"
+                speed={2}
+                delay={0}
+                color="#904aff"
+                shineColor="#ffffff"
+                spread={120}
+                direction="left"
+                yoyo={false}
+                pauseOnHover={false}
+                disabled={false}
+                className="font-inter text-2xl font-bold "
+              />
+            </div>
+            <div className="hidden md:flex">
+              <ShinyText
+                text="Let's Work Together"
+                speed={2}
+                delay={0}
+                color="#904aff"
+                shineColor="#ffffff"
+                spread={120}
+                direction="left"
+                yoyo={false}
+                pauseOnHover={false}
+                disabled={false}
+                className="font-inter text-5xl font-bold"
+              />
+            </div>
+            <div className="w-10 border-t-2 border-dashed mt-4 mb-12 border-secondary"></div>
             <p className="flex-col flex-wrap wrap-break font-sora text-secondary sm:text-lg">
               Have a project in mind or an idea you’d like to bring to life? I’d
               love to hear about it. Let’s create something impactful together.
             </p>
           </div>
           {/* 2nd Col */}
-          <div className="flex flex-col w-full md:w-[calc(100% - 12px)] text-md font-bold py-6 space-y-10 pt-10 md:text-lg md:pt-0">
+          <div className="flex flex-col w-full md:w-[calc(100% - 12px)] text-md font-bold py-6 space-y-8 pt-10 md:text-lg md:pt-0">
             <div className="flex-col justify-start align-middle items-center pt-6 space-y-6">
               <div className="flex w-full gap-6 text-secondary items-center">
                 <IoCall className="w-6 h-6 text-secondary" />
